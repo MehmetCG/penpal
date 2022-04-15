628b7eedd5730eb9fe590b07e33b4111ebc02b2b
mehmetcan <mehmetcan@artistanbul.io>
2022-04-15 20:46:45 +0300

    render templates and static files

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path("home/", HomeView.as_view(), name="home" ),
+    path("login/", LoginView.as_view(), name="login" )
 ]
